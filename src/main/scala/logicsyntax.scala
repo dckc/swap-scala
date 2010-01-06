b27bd62250118ed1ab0fe95bc1dbd3c5b94ca4da
Dan Connolly <http://www.w3.org/People/Connolly/@localhost>
2010-01-06 16:35:31 -0600

    code compiles!

--- src/main/scala/logicsyntax.scala
+++ src/main/scala/logicsyntax.scala
@@ -4,28 +4,29 @@
 package org.w3.swap.logicalsyntax
 
 /* cf http://en.wikipedia.org/wiki/First-order_logic#Terms */
-sealed case class Term
-case class Variable extends Term
-case class FunctionSymbol
+sealed case class Term()
+case class Variable() extends Term
+case class FunctionSymbol()
 case class Apply(f: FunctionSymbol, terms: List[Term]) extends Term {
-  override def toString(): String {
+  override def toString(): String = {
     f.toString + "@" + terms.toString
   }
+}
 
 /* http://en.wikipedia.org/wiki/First-order_logic#Formulas */
-sealed case class Formula
+sealed case class Formula()
 case class TruthConstant(b: Boolean) extends Formula
 case class PredicateSymbol(name: String)
 case class Atom(p: PredicateSymbol, terms: List[Term]) extends Formula {
-  override def toString(): String {
+  override def toString(): String = {
     p.toString() + terms.toString()
   }
 }
 case class Equals(x: Term, y: Term) extends Formula
 case class Not(f: Formula) extends Formula
 case class And(f: Formula, g: Formula) extends Formula
-case class Exists(v: Variable, f: Formula)
-case class Forall(v: Variable, f: Formula)
+case class Exists(v: Variable, f: Formula) extends Formula
+case class Forall(v: Variable, f: Formula) extends Formula
 
 /* TODO: consider http://en.wikipedia.org/wiki/Intuitionistic_logic */
 // object bottom: Formula
@@ -34,8 +35,8 @@ object Notation {
   def or(f: Formula, g: Formula) { Not(And(Not(f), Not(g))) }
   def implies(f: Formula, g: Formula) { Not(And(Not(f), g)) }
 
-  def variables(a: Atom): List[Variable] {
-    a.terms.filter(_ match { case Variable => true; case _ => false } )
+  def variables(a: Atom): List[Variable] = {
+    a.terms flatMap {case v : Variable => List(v); case _ => Nil}
   }
 }
 
--- src/main/scala/rdf2004.scala
+++ src/main/scala/rdf2004.scala
@@ -4,14 +4,18 @@
 
 package org.w3.swap.rdf2004
 
-import org.w3.swap.logicalsyntax.{Formula, PredicateSymbol, Constant, Variable, variables}
+import org.w3.swap.logicalsyntax.{Formula, TruthConstant, Atom, And, Exists,
+				  PredicateSymbol,
+				  Term, Apply, Variable,
+				  FunctionSymbol,
+				  Notation}
 
 /* Terms */
 case class URI(i: String) /* ISSUE: not every string makes a URI */
           /* ISSUE: actually closer to IRI; called RDFuri or something
 	   * in the spec */
      extends FunctionSymbol {
-  override def toString(): String {
+  override def toString(): String = {
     "<" + i + ">"
   }
 }
@@ -19,59 +23,70 @@ sealed case class Literal() extends FunctionSymbol
 case class PlainLiteral(s: String) extends Literal
 case class Language(code: String) /* ISSUE: restricted to lang code syntax */
 case class Text(chars: String, lang: Language) extends Literal {
-  override def toString(): String {
+  override def toString(): String = {
     "'" + chars + "'@" + lang.code
   }
 }
 case class DatatypedLiteral(value: String, dt: URI) extends Literal {
-  override def toString(): String {
-    "'" + chars + "'^^<" + dt.i + ">"
+  override def toString(): String = {
+    "'" + value + "'^^<" + dt.i + ">"
   }
 }
 
 
 case class BlankNode(hint: String, id: AnyRef) extends Variable {
-  override def toString(): String {
+  override def toString(): String = {
     "_:" + hint
   }
 }
+
+class SyntaxError(msg: String) extends Exception
 
 /* Formulas */
 object AbstractSyntax {
   val holds = PredicateSymbol("holds")
 
-  implicit def makeTerm(f: FunctionSymbol): Term { Apply(f, Nil) }
+  implicit def makeTerm(f: FunctionSymbol): Term = { Apply(f, Nil) }
 
   /* checks well-formedness of Atoms */
-  def triple(s: Term, p: Term, o: Term) {
+  def triple(s: Term, p: Term, o: Term) = {
     p match {
       case Apply(_: URI, Nil) =>
 	s match {
 	  /* is there a scala syntax for folding 2 cases? */
-	  case BlankNode => Atom(holds, [s, p, o])
-	  case Apply(_: URI, Nil) => Atom(holds, [s, p, o])
-	  case _ => raise SyntaxError("subject must be URI or Blank Node")
+	  case BlankNode(_, _) => Atom(holds, List(s, p, o))
+	  case Apply(_: URI, Nil) => Atom(holds, List(s, p, o))
+	  case _ => throw new SyntaxError("subject must be URI or Blank Node")
 	}
-      case _ => raise SyntaxError("predicate must be URI")
+      case _ => throw new SyntaxError("predicate must be URI")
     }
   }
 
-  def variables(atoms: List[Atom]): List[Variable] {
-    atoms.flatMap(_ => variables(_))
+  def variables(atoms: List[Atom]): List[Variable] = {
+    atoms.flatMap(a => Notation.variables(a))
   }
 
-  def quantify(f: Formula, vars: List[Variable]): Formula {
+  def quantify(f: Formula, vars: List[Variable]): Formula = {
     if (vars == Nil) { f }
     else { quantify(Exists(vars.head, f), vars.tail) }
   }
-      
 
-  def graph(triples: List[Atom]): Formula {
+  def unquantify(f: Formula): (List[Variable], Formula) = {
+    f match {
+      case Exists(v, g) => {
+	val (rest, gg) = unquantify(g)
+	(v :: rest, gg)
+      }
+      case _ => (List(), f)
+    }
+  }
+
+  def graph(triples: List[Atom]): Formula = {
     if (triples == Nil) { TruthConstant(true) }
     else {
-      val sorted = triples.sort(compare)
+      val sorted = triples.sort((a, b) => (compare(a, b) <= 0))
       
-      def conjoin(todo: List[Atom], done: Formula): Formula {
+      def conjoin(todo: List[Atom], done: Formula): Formula = {
 	if (todo == Nil) { done }
 	else { conjoin(todo.tail, And(todo.head, done)) }
       }
@@ -82,15 +97,15 @@ object AbstractSyntax {
 
   val graph0 = TruthConstant(true)
 
-  def + (graph: Formula, s, p, o: Term): Formula {
+  def + (graph: Formula, s: Term, p: Term, o: Term): Formula = {
     val t = triple(s, p, o)
-    val vars, conjunction = unquantify(graph)
-    quantify(And(t, conjunction), vars + variables(t))
+    val (vars, conjunction) = unquantify(graph)
+    quantify(And(t, conjunction), vars ++ Notation.variables(t))
   }
 
   /*
    * ISSUE: keep the triples sorted for ease of graph comparison?
    */
-  def compare (x: Atom, y: Atom) { compare(x.hashCode(), y.hashCode()) }
+  def compare (x: Atom, y: Atom): Int = { x.hashCode().compare(y.hashCode()) }
 
 }
