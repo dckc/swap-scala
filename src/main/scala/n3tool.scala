1bfd200c74ff9f62253a1d3b07f5eeabcce4d74e
Dan Connolly <http://www.w3.org/People/Connolly/@localhost>
2010-01-12 13:39:20 -0600

    handle file not found in n3tool

--- src/main/scala/n3tool.scala
+++ src/main/scala/n3tool.scala
@@ -1,31 +1,39 @@
 package org.w3.swap
 
-import java.io.FileReader
+import java.io.{FileReader, FileNotFoundException}
 
 object N3Tool {
   def main(args: Array[String]): Unit = {
     if (args.length != 1) {
       println("Usage: n3parse <file>")
-      return
+      return // TODO: non-0 return value
     }
 
     val base = "data:@@" // TODO: learn how to get the directory of a file
     val parser = new N3Parser(base)
-    val reader = new FileReader(args(0))
-    val result = parser.parseAll(parser.document, reader)
+    try {
+      val reader = new FileReader(args(0))
+      val result = parser.parseAll(parser.document, reader)
 
-    result match {
-      case parser.Success(f, _ ) => println(f.quote().print())
-      case parser.Failure(x, y) => {
-	println("N3Parser failure:")
-	println(x)
-	println(y.pos.longString)
+      result match {
+	case parser.Success(f, _ ) => println(f.quote().print())
+	case parser.Failure(x, y) => {
+	  println("N3Parser failure:")
+	  println(x)
+	  println(y.pos.longString)
+	}
+	case parser.Error(x, y) => {
+	  println("N3Parser error:")
+	  println(x)
+	  println(y)
+	  None
+	}
       }
-      case parser.Error(x, y) => {
-	println("N3Parser error:")
-	println(x)
-	println(y)
-	None
+    } catch {
+      // TODO: diagnostics to stderr
+      case e: FileNotFoundException => {
+	println(e)
+	return
       }
     }
   }
