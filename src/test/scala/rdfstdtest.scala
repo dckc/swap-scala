d47f89b445b7dd6b8460a35f57cf0cb9f3e0cf80
Dan Connolly <http://www.w3.org/People/Connolly/@localhost>
2010-02-05 17:39:18 -0600

    move WebData from swap.test to swap

--- src/test/scala/rdfstdtest.scala
+++ src/test/scala/rdfstdtest.scala
@@ -3,6 +3,7 @@ package org.w3.swap.test
 import org.w3.swap
 import swap.logic.{Formula, Exists, And, Atomic, Term, Literal}
 import swap.rdf.{URI, BlankNode, Graph, Holds}
+import swap.WebData
 
 abstract class Namespace(n: String) {
   protected def term(name: String): URI = {
@@ -253,86 +254,3 @@ object Runner {
   }
 }
 
-/**
- * WebData can read RDF in various formats.
- */
-object WebData {
-  import java.io.InputStreamReader
-  import scala.xml.XML
-
-  // TODO: conneg
-
-  def loadRDFXML(addr: String): Formula = {
-    import org.w3.swap.rdf.RDFXMLParser
-
-    val e = XML.load(addr)
-    val p = new RDFXMLParser(addr) // @@TODO: absolutize base
-    p.parse(e)
-  }
-
-  protected def content(addr: String): InputStreamReader = {
-    val conn = new java.net.URL(addr).openConnection()
-    new InputStreamReader(conn.getInputStream())
-  }
-
-  /**
-   * @throws IOException if openConnection(addr) throws one
-   */
-  def loadNT(addr: String): Formula = {
-    val p = new swap.ntriples.NTriplesParser()
-    p.parseAll(p.ntripleDoc, content(addr)) match {
-      case p.Success(f, _) => f
-      case failure => {
-	throw new Exception("@@parse failure:" + failure)
-      }
-    }
-  }
-
-  /**
-   * @throws IOException if openConnection(addr) throws one
-   */
-  def loadTurtle(addr: String): Formula = loadTurtle(addr, addr)
-
-  /**
-   * @throws IOException if openConnection(addr) throws one
-   */
-  def loadTurtle(addr: String, base: String): Formula = {
-    val p = new swap.TurtleParser(base)
-    p.parseAll(p.document, content(addr)) match {
-      case p.Success(f, _) => f
-      case failure => {
-	throw new Exception("@@parse failure:" + failure)
-      }
-    }
-  }
-
-  def loadSPARQL(addr: String): Formula = {
-    val p = new swap.sparql.SPARQLParser(addr)
-
-    p.parseAll(p.AskQuery, content(addr)) match {
-      case p.Success(f, _) => f
-      case failure => {
-	println("SPARQL @@parse failure:")
-	println(failure)
-	null
-      }
-    }
-  }
-    
-  def loadRDFa(addr: String): Formula = {
-    import org.w3.swap.rdf.RDFaParser
-
-    val e = XML.load(addr)
-    val base = e \ "head" \ "base" \ "@href"
-    val p = new RDFaParser(if (base.isEmpty) addr else base.text)
-    p.parse(e)
-  }
-
-  /**
-   * Convert a (local) file path to a URI.
-   */
-  def asURI(fp: String): String = {
-    val cwd = java.lang.System.getProperty("user.dir")
-    new java.io.File(cwd, fp).toURI().toString()
-  }
-}
