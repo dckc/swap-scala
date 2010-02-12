73ece5ced70e3c0502daa9727361cac28b89ba19
Dan Connolly <http://www.w3.org/People/Connolly/@localhost>
2010-02-12 17:44:10 -0600

    ECLogic closed, bottom, horn (untested)

--- src/main/scala/coherent.scala
+++ src/main/scala/coherent.scala
@@ -16,6 +16,23 @@ import swap.logic1.{Term, Variable}
  */
 abstract class CoherentLogic extends FormalSystem {
 
+  /**
+   * Does the conclusion consist of a single Atom?
+   */
+  def horn(f: CLFormula): Boolean = {
+    f match {
+      /* this is only split for line length. hmm. */
+      case Implication(_, Disjunction(List(e1))) => e1 match {
+	case Exists(Nil, Conjunction(List(atom))) => true
+	case _ => false
+      }
+      case _ => false
+    }
+  }
+
+  def variables(f: CLFormula): Set[Variable] //TODO
+  def closed(f: CLFormula) = variables(f).isEmpty
+  val bottom = Disjunction(Nil)
 }
 
 /**
@@ -23,7 +40,7 @@ abstract class CoherentLogic extends FormalSystem {
  */
 sealed abstract class CLFormula extends Formula
 case class Atomic(rel: Symbol, args: List[Term]) extends CLFormula
-case class Implicaton(c: Conjunction, d: Disjunction) extends CLFormula
+case class Implication(c: Conjunction, d: Disjunction) extends CLFormula
 case class Conjunction(ai: List[Atomic]) extends CLFormula
 case class Disjunction(ei: List[Exists]) extends CLFormula
 case class Exists(xi: List[Variable], c: Conjunction) extends CLFormula
